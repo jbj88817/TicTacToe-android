82143d133adad898205e815315e90b12d2213022
Bojie Jiang <jiangbojie@gmail.com>
2015-10-29 15:06:22 -0700

    add checking winning

--- app/src/main/java/com/bojie/tictactoe_android/MainActivity.java
+++ app/src/main/java/com/bojie/tictactoe_android/MainActivity.java
@@ -2,6 +2,7 @@ package com.bojie.tictactoe_android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -10,7 +11,15 @@ public class MainActivity extends AppCompatActivity {
 
     // 0 = yellow, 1 = red
     int activePlayer = 0;
-    
+
+    // 2 means unplayed
+    int[] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2};
+
+    int[][] winningPositions = {{0, 1, 2}, {3, 4, 5},
+            {6, 7, 8}, {0, 3, 6},
+            {1, 4, 7}, {2, 5, 8},
+            {0, 4, 8}, {2, 4, 6}};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,16 +30,34 @@ public class MainActivity extends AppCompatActivity {
 
         ImageView counter = (ImageView) view;
 
-        counter.setTranslationY(-1000f);
-        if (activePlayer == 0) {
-            counter.setImageResource(R.drawable.yellow);
-            activePlayer = 1;
-        } else {
-            counter.setImageResource(R.drawable.red);
-            activePlayer = 0;
+        Log.d("TAG", counter.getTag().toString());
+
+        int tappedCounter = Integer.parseInt(counter.getTag().toString());
+
+        if (gameState[tappedCounter] == 2) {
+
+            gameState[tappedCounter] = activePlayer;
+
+            counter.setTranslationY(-1000f);
+            if (activePlayer == 0) {
+                counter.setImageResource(R.drawable.yellow);
+                activePlayer = 1;
+            } else {
+                counter.setImageResource(R.drawable.red);
+                activePlayer = 0;
+            }
+
+            counter.animate().translationYBy(1000f).rotation(360f).setDuration(300);
+
+            for (int[] winningPosition : winningPositions) {
+                if (gameState[winningPosition[0]] == gameState[winningPosition[1]] &&
+                        gameState[winningPosition[1]] == gameState[winningPosition[2]] &&
+                        gameState[winningPosition[0]] != 2) {
+                    Log.d("winning", gameState[winningPosition[0]]+"");
+                }
+            }
         }
 
-        counter.animate().translationYBy(1000f).rotation(360f).setDuration(300);
 
     }
 }
